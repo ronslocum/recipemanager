53951eebde1b004ae3c454acfbcddbd778639c7a
Ron Slocum <slocumro@gmail.com>
2015-11-08 16:15:26 -0800

    Add Admin functionality

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,6 +3,7 @@ class RecipesController < ApplicationController
   before_action :require_user, except: [:show, :index, :like]
   before_action :require_user_like, only: [:like]
   before_action :require_same_user, only: [:edit, :update]
+  before_action :admin_user, only: [:destroy]
 
   def index
     @recipes = Recipe.paginate(page: params[:page], per_page: 4)
@@ -52,6 +53,12 @@ class RecipesController < ApplicationController
     redirect_to :back
   end
 
+  def destroy
+    Recipe.find(params[:id]).destroy
+    flash[:success] = "Recipe deleted."
+    redirect_to recipes_path
+  end
+
   private
     def recipe_params
       params.require(:recipe).permit(:name, :summary, :description, :picture, style_ids: [], ingredient_ids: []) #this white lists the parameters that can come through
@@ -62,16 +69,21 @@ class RecipesController < ApplicationController
     end
 
     def require_same_user
-      if current_user != @recipe.chef
+      if current_user != @recipe.chef && !current_user.admin?
         flash[:danger] = "You can only edit your own recipes!"
         redirect_to root_path
       end
     end
 
     def require_user_like
-    if !logged_in?
-      flash[:danger] = "You must be logged in to perform this action."
-      redirect_to :back
+      if !logged_in?
+        flash[:danger] = "You must be logged in to perform this action."
+        redirect_to :back
+      end
     end
+
+
+  def admin_user
+    redirect_to recipes_path unless current_user.admin?
   end
 end
